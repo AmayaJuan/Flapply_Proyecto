71a77c2fdefd1707e31ca696f6a96164b98bbe1a
AmayaJuan <jpva360@gmail.com>
2017-07-28 16:33:04 -0500

    Tutorial 02
    Haciendo que vuele

---
+++ Flappy/Assets/FlappyGame/Script/Bird.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    bool isDead;
+    float upForce = 200f;
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        if (isDead) return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * upForce);
+        }
+        
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        isDead = true;
+
+    }
+}
