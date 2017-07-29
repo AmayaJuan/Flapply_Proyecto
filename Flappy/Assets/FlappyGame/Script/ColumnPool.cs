6dd5b35f36e079c263d46b0079fe103adf7b6bdb
AmayaJuan <jpva360@gmail.com>
2017-07-28 21:27:30 -0500

    Tutorial 08
    Reciclado de objectos

---
+++ Flappy/Assets/FlappyGame/Script/ColumnPool.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour
+{
+    public int columnPoolSize = 5;
+    public GameObject columnPrefab;
+    
+
+    GameObject[] columns;
+    Vector2 objectPoolPosition = new Vector2(-14, 0);
+    int currentColumn;
+    float timeSinceLastSpawned;
+    float columnMin = -2.9f;
+    float columnMax = 1.4f;
+    float spawXPosition = 10f;
+    float spawRate;
+
+    void Start ()
+    {
+        columns = new GameObject[columnPoolSize];
+        for (int i = 0; i < columnPoolSize; i++)
+            columns[i] = Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+	}
+	
+	void Update ()
+    {
+        spawRate = Random.Range(4, 6);
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if (!GameController.instance.gameOver && timeSinceLastSpawned > spawRate)
+        {
+            timeSinceLastSpawned = 0;
+            float spawYPosition = Random.Range(columnMin, columnMax);
+            columns[currentColumn].transform.position = new Vector2(spawXPosition, spawYPosition);
+            currentColumn++;
+            if (currentColumn >= columnPoolSize)
+                currentColumn = 0;
+        }
+	}
+}
